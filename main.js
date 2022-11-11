f45f336e9191eb579eaa85909ff2ea848dd2a095
Ch1lim <Ch1lim@yandex.ru>
2022-11-11 13:22:37 +0300

    third

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ document.getElementById("result_btn").onclick = function(){
     let num1 = Number(document.getElementById("value1").value);
     let num2 = Number(document.getElementById("value2").value);
     let operator = document.getElementById("operation").value;
+    let list = document.querySelector(".list_output");
     let result = 0;
     switch (operator) {
         case "+":
@@ -18,7 +19,17 @@ document.getElementById("result_btn").onclick = function(){
             break;
     }
 
-    document.getElementById("result_block").innerHTML = Number(result.toFixed(14));
+    result = Number(result.toFixed(14));
+    document.getElementById("result_block").innerHTML = result;
+
+    let div = document.createElement('div');
+    div.className = "item_output";
+    div.innerHTML = result;
+    list.append(div);
+    div.onclick = function(){
+        div.remove();
+    }
+
 };
 
 
